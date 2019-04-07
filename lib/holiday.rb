7f1b4e0cf40d4b0ebf318bac90c28b23966128be
Peter Viss <peter.c.viss@biola.edu>
2019-04-07 19:03:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,14 +65,14 @@ def all_supplies_in_holidays(holiday_hash)
             final_holiday << x.capitalize!
           end
         holiday = final_holiday.join(" ")
-  
+
          supply = supply.join(", ")
         puts "  #{holiday}: #{supply}"
-  
+
        end
     end
-  
-  
+
+
    end
 
 def all_holidays_with_bbq(holiday_hash)
